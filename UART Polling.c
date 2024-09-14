01fbf0cae9ae066b7be052f73288a6c7c3c5753b
MOHAN SINGIREDDI <125856165+mohan18794@users.noreply.github.com>
2024-09-14 16:30:25 +0530

    Update UART Polling.c

--- UART Polling.c
+++ UART Polling.c
@@ -1,1 +1,18 @@
-
+#include<lpc21xx.h>
+void uart()
+{
+	PINSEL0=0x05;
+	U0LCR=0x83;
+	U0DLL=97;
+	U0LCR=0x03;
+}
+void main(){
+	int x;
+	uart();
+	while(1){
+		while(!(U0LSR &0x01));
+		x=U0RBR;
+		U0THR=x;
+		while(! (U0LSR &0x20));
+	}
+}
